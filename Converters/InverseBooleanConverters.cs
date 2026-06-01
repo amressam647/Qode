1b6a1c2a35edec705069b15fc0dbd0b2e80b4256
amressam647 <41263002+amressam647@users.noreply.github.com>
2026-06-01 15:03:28 +0300

    Add files via upload

--- Converters/InverseBooleanConverters.cs
+++ Converters/InverseBooleanConverters.cs
@@ -1,1 +1,83 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
 
+namespace LocalCursor.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+    }
+
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b && b ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility v && v == Visibility.Collapsed;
+        }
+    }
+
+    public class EmptyToDefaultConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = value?.ToString() ?? "";
+            var def = parameter?.ToString() ?? "";
+            return string.IsNullOrWhiteSpace(s) ? def : s;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>Int count -> "X في الانتظار" for queue header.</summary>
+    public class QueueCountToHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var n = value is int i ? i : 0;
+            return n + " في الانتظار";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>Empty string -> Collapsed, non-empty -> Visible.</summary>
+    public class EmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isEmpty = string.IsNullOrEmpty(value?.ToString());
+            bool inverted = parameter?.ToString() == "Inverted";
+
+            if (inverted)
+                return isEmpty ? Visibility.Visible : Visibility.Collapsed;
+            
+            return isEmpty ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
